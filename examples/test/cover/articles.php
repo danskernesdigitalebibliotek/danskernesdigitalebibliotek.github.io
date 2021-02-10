a9f64e79a6bf591d635df0134f83abc72c02aecf
DDBdrift <22028109+DDBdrift@users.noreply.github.com>
2021-02-10 16:55:17 +0100

    Separate dir for logo files. updated doc

--- examples/test/cover/articles.php
+++ examples/test/cover/articles.php
@@ -1,6 +1,19 @@
 <?php
+/**
+ *  @file       articles.php
+ *  @brief      Setting default cover on articles
+ *  
+ *  @details    Default cover build using meta data from JSON record list and simple logo covers,
+ *  Requires meta data in articles.json and styling in mystyle.css 
+ *  @copyright  http://www.gnu.org/licenses/lgpl.txt LGPL version 3
+ *  @author     Erik Bachmann <ErikBachmann@ClicketyClick.dk>
+ *  @since      2021-02-10T16:30:03
+ *  @version    2021-02-10T16:34:07
+ */
 
+// Reading meta data
 $data   = json_decode( file_get_contents( "articles.json" ), JSON_OBJECT_AS_ARRAY );
+// Template for formatting meta data
 $template   = [
     [ 'typeBibDKType'   => '[%s]<br>' . PHP_EOL ],
     [ 'dcTitleFull'   => '<span class="title">%s</span>' . PHP_EOL ],
@@ -8,7 +21,6 @@ $template   = [
     [ 'creator'   => '<br><span class="creator">%s</span>' . PHP_EOL ],
     [ 'abstract'   => '<dir class="abstract">%s</dir>' . PHP_EOL ],
     [ 'subject'   => 'Emneord: <dir class="subject">%s</dir>' . PHP_EOL ],
-
     [ 'dcTitleFull'   => '<dl>' . PHP_EOL ],
     [ 'dcLanguage'   => '<dt>Sprog<dd>%s</dd>' . PHP_EOL ],
     [ 'subjectGenre'   => '<dt>Genre<dd>%s</dd>' . PHP_EOL ],
@@ -16,39 +28,36 @@ $template   = [
     [ 'audience'   => '<dt>Målgruppe<dd>%s</dd>' . PHP_EOL ],
     [ 'partOf'   => '<dt>Findes i<dd>%s</dd>' . PHP_EOL ],
     [ 'dcTitleFull'   => '</dl>' . PHP_EOL ],
-
 ];
 
 $data = $data['data'];
 
+// File header
 echo '<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8" />
         <meta name="author" content="MarkdownViewer++" />
         <title>articles.md</title>
-
-		<link rel="stylesheet" href="mystyle.css">
-</head>
+        <link rel="stylesheet" href="mystyle.css">
+    </head>
 <body>
 
 ';
 
-
-
+// Build each record block
 foreach ( $data as $record => $entries ) {
     $output     = formatRecord( $entries );
     echo "$output\n<br clear=both>\n";
-    //printf( "<h2>%s</h2>\n", implode( " ; ", $entries["dcTitleFull"] ) );
 }
 
-
-
+//----------------------------------------------------------------------
 
 function formatRecord( $entries ) {
     global $template;
     $output = "";
 
+    //Source ID for cover if not found on Cover Service
     $tags    = ['isPartOfISSN', 'identifierISSN', 'acSource' ];
     foreach ($tags as $tag ) {
         if ( ! empty($entries[$tag]) ) {
@@ -56,29 +65,26 @@ function formatRecord( $entries ) {
             $output .= sprintf( "
             <hr clear=both>
             <div class='container'>
-                <a href='%s'><img src='%s.jpg' class='logo'></a>
+                <a href='%s'><img src='logo/%s.jpg' class='logo'></a>
                 <div class='bottom-left'>
                     <span class='title overlayentity'>%s</span><span class='release overlayentity'>%s</span><span class='creator overlayentity'>%s</span>
                 </div>
-                <div class='overlay'>%s</div>
+                <div class='overlay'>%s [%s]</div>
             </div>
             "
-//,   implode( " ; ", $entries['hasOnlineAccess'] )
 ,   implode( " ; ", ( ! empty($entries['hasOnlineAccess']) ? $entries['hasOnlineAccess'] : [] ) )
 ,   strtolower(implode( " ; ", $entries[$tag] ))
 ,   implode( " ; ", $entries['dcTitleFull'] )
 ,   implode( " ; ", $entries['date'] )
-//,   implode( " ; ", $entries['creator'] )
 ,   ucwords(implode( " ; ", ( ! empty($entries['creator']) ? $entries['creator'] : [] ) ),  " \t\r\n\f\v-_")
 ,   implode( " ; ", ( ! empty($entries['partOf']) ? $entries['partOf'] : $entries['acSource'] ) )
+,   $tag
 );
             break;
         }
     }
 
     foreach ( $template as $field ) {
-        //var_export($field);
-        
         foreach ( $field as $tag => $form ) {
             if ( ! empty($entries[$tag]) )
                 $output .= sprintf( $form, implode( " ; ", $entries[$tag] ) );
@@ -86,5 +92,6 @@ function formatRecord( $entries ) {
         
     }
     return( $output );
-}
+}   // formatRecord
+
 ?>
